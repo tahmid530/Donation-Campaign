2680edc13117b603ffcdcd2a118808c23312b99e
tahmid530 <tahmidahmed530@gmail.com>
2023-09-27 23:50:47 +0600

    more bug fixed

--- src/Components/Pages/SingleCard/CardShow.jsx
+++ src/Components/Pages/SingleCard/CardShow.jsx
@@ -2,19 +2,19 @@ import { useLoaderData, useParams } from "react-router-dom";
 import swal from "sweetalert";
 
 const CardShow = () => {
-const{id}  = useParams()
-const cards = useLoaderData()
-const card = cards.find(card=>card.id==id)
+    const { id } = useParams()
+    const cards = useLoaderData()
+    const card = cards.find(card => card.id == id)
     console.log(cards)
-    const {img,card_title,button_bg_color,btn_color,price,description}=card;
+    const { img, card_title, button_bg_color, btn_color, price, description } = card;
 
-    const handleDonation= ()=>{
+    const handleDonation = () => {
 
         const donationArray = [];
 
         const donationItems = JSON.parse(localStorage.getItem('donation'))
 
-        if(!donationItems){
+        if (!donationItems) {
             donationArray.push(card)
             localStorage.setItem('donation', JSON.stringify(donationArray))
             swal("Good job!", "Donation Added!", "success");
@@ -22,22 +22,21 @@ const card = cards.find(card=>card.id==id)
 
         else {
 
-           // eslint-disable-next-line react/prop-types
-           const isExit = donationItems.find(card => card.id == id)
-           
-
-           if(!isExit){
-
-            donationArray.push(...donationItems, card)
-            localStorage.setItem('donation', JSON.stringify(donationArray))
-            swal("Good job!","Donation Added!", "success");
- 
-           }
+            const isExit = donationItems.find(card => card.id == id)
 
 
-           else{
-            swal("Error!","No Duplicate!", "error");
-           }
+            if (!isExit) {
+
+                donationArray.push(...donationItems, card)
+                localStorage.setItem('donation', JSON.stringify(donationArray))
+                swal("Good job!", "Donation Added!", "success");
+
+            }
+
+
+            else {
+                swal("Error!", "No Duplicate!", "error");
+            }
 
 
 
@@ -45,30 +44,30 @@ const card = cards.find(card=>card.id==id)
     }
 
     return (
-        
-            <div>
-             <div >
-           <div className="card card-compact h-[50vh] w-full
-            bg-base-100 shadow-xl">
-        <div>
-        <figure className='relative '>
-            <div className='bg-black absolute w-full h-24 top-[500px]'>
-            
-            </div>
-            <img  className="w-[1320px] h-[590px] bg-black opacity-60 " src={img} alt="Shoes" />
-            </figure>
 
-            <button onClick={handleDonation} style={{background: button_bg_color , text:btn_color }}  className="btn btn-primary text-[#FFF] absolute top-[520px] left-8 border-none">Donate ${price}</button>
+        <div>
+            <div >
+                <div className="card card-compact h-[50vh] w-full
+            bg-base-100 shadow-xl">
+                    <div>
+                        <figure className='relative '>
+                            <div className='bg-black absolute w-full h-24 top-[501px]'>
+
+                            </div>
+                            <img className="w-[1320px] h-[590px] bg-black opacity-60 " src={img} alt="Shoes" />
+                        </figure>
+
+                        <button onClick={handleDonation} style={{ background: button_bg_color, text: btn_color }} className="btn btn-primary text-[#FFF] absolute top-[520px] left-8 border-none">Donate ${price}</button>
+                    </div>
+                    <div className="card-body">
+                        <h2 className="card-title text-4xl font-extrabold">{card_title}</h2>
+                        <p className="font-none">{description}</p>
+
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="card-body">
-            <h2 className="card-title text-4xl font-extrabold">{card_title}</h2>
-            <p className="font-none">{description}</p>
-            
-        </div>
-     </div>
-        </div>
-        </div>
-       
+
     );
 };
 
